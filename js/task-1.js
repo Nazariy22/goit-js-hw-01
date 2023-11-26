06588ff1c0704a307f875e05a7ac07c7290914e1
Nazariy22 <nazariy.vintoniak@gmail.com>
2023-11-26 13:34:25 +0200

    done

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,15 @@
-function getAllPropValues(propName) {
-  const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
-  for (const product of products) {
-    if (product === propName) {
-      propName.product
-    }
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+  for (const quantity of Object.values(products)) {
+    totalQuantity += quantity;
   }
-  return [];
+  return totalQuantity <= containerSize;
 }
-console.log(getAllPropValues("name"));
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
